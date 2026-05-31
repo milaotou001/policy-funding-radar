7bf3751df429f58b435cf3fea8e1e83aa0f91c3f
milaotou001 <1402204668@qq.com>
2026-05-31 22:42:07 +0800

    chore: add market signal merge script, remove temp audit artifact

---
+++ scripts/merge-market.cjs
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+const industries = JSON.parse(fs.readFileSync(path.join(__dirname, 'data', 'industries.json'), 'utf8'));
+const signals = JSON.parse(fs.readFileSync(path.join(__dirname, 'data', 'market-signals.json'), 'utf8'));
+const output = industries.industries.map(ind => {
+  const sig = signals[ind.id];
+  return sig ? { ...ind, market_signal: sig } : ind;
+});
+fs.writeFileSync(path.join(__dirname, 'data', 'industries.json'), JSON.stringify({ ...industries, industries: output }, null, 2), 'utf8');
+const withSignal = output.filter(i => i.market_signal);
+console.log(`Merged ${withSignal.length} market signals.`);
